364785bb2c6e8823169187f2165ef0940423dca0
Vanessa Buzzell <vanessabuzzell@live.com>
2025-05-16 00:59:43 -0400

    added an apiKey

--- controllers/temple.js
+++ controllers/temple.js
@@ -2,7 +2,7 @@ const db = require('../models');
 const Temple = db.temples;
 
 const apiKey =
-  'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
+  '9ff959135374d793cd9e7e239091fd84';
 
 exports.create = (req, res) => {
   // Validate request
